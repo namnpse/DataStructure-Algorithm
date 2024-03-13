06b2010cc405e5bd74344878a50dbc662adcbb2d
Namnpse <nam444479@gmail.com>
2024-03-13 11:31:12 +0700

    [DP] resolve EditDistance_72 problem

---
+++ dsajava/src/main/java/com/leetcode/dsajava/my_daily_leetcode_challenge/dynamic_programming/EditDistance_72.java
@@ -0,0 +1,12 @@
+package com.leetcode.dsajava.my_daily_leetcode_challenge.dynamic_programming;
+
+public class EditDistance_72 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int minDistance(String word1, String word2) {
+        return 1;
+    }
+}
