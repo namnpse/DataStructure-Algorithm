d29862a3cf10e9cc0e263f36c91770e073c53e90
Namnpse <nam444479@gmail.com>
2024-04-22 00:35:41 +0700

    [50 common prob] resolve WayToClimb_32 using DP

--- dsajava/src/main/java/com/leetcode/dsajava/course/common_50_probs/WayToClimb_32.java
+++ dsajava/src/main/java/com/leetcode/dsajava/course/common_50_probs/WayToClimb_32.java
@@ -9,6 +9,7 @@ public class WayToClimb_32 {
     }
 
     // Section 32:
+    // C1: O(m^n), Space: O(n)
     public static int waysToClimb(int n, Set<Integer> possibleSteps) {
         if (n == 0) return 1;
         int steps = 0;
@@ -18,5 +19,20 @@ public class WayToClimb_32 {
             }
         }
         return steps;
+    }
+
+    // C2: O(n*m), Space: O(1)
+    public static int waysToClimb2(int n, Set<Integer> possibleSteps) {
+        // your code here
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int step : possibleSteps) {
+                if (i - step >= 0) {
+                    dp[i] += dp[i - step];
+                }
+            }
+        }
+        return dp[n];
     }
 }
