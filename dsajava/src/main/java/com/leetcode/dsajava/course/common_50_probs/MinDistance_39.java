29e13431ca0ddc21ba498c7a5de6830be94cf0d9
Namnpse <nam444479@gmail.com>
2024-05-05 10:18:52 +0700

    [50 common prob] resolve MinDistance_39 using Dynamic Programming

--- dsajava/src/main/java/com/leetcode/dsajava/course/common_50_probs/MinDistance_39.java
+++ dsajava/src/main/java/com/leetcode/dsajava/course/common_50_probs/MinDistance_39.java
@@ -32,4 +32,31 @@ public class MinDistance_39 {
         memoiz.put(key, min);
         return min;
     }
+
+    // C2: dynamic programming
+    public int minDistance2(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        dp[0][0] = 0;
+        for (int i = 1; i <= n; i++) dp[i][0] = i;
+        for (int i = 1; i <= m; i++) dp[0][i] = i;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(
+                            Math.min(
+                                    dp[i - 1][j], // remove
+                                    dp[i][j - 1] // add
+                            ),
+                            dp[i - 1][j - 1] // replace
+                    );
+                }
+            }
+        }
+        return dp[n][m];
+    }
 }
